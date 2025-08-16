c4a5da03d1e2c2d0e1f7f6a2b5a0d4dac338e713
박성훈 / Koder <koder0205@gmail.com>
2025-08-17 01:06:02 +0900

    feat: send chat in laftel

--- src/contents/chatting.tsx
+++ src/contents/chatting.tsx
@@ -1,9 +1,10 @@
 import type { PlasmoCSConfig, PlasmoGetInlineAnchor } from "plasmo"
 import cssText from "data-text:../style.css"
-import { LuMessageSquare } from "react-icons/lu"
-import { useEffect } from "react"
+import { LuMessageSquare, LuSend } from "react-icons/lu"
+import { useEffect, useState } from "react"
 import { useStorage } from "@plasmohq/storage/hook"
 import type { Chat } from "~background/const"
+import { message } from "~popup/message"
 
 export const config: PlasmoCSConfig = {
   matches: ["*://laftel.net/*"]
@@ -20,12 +21,62 @@ export const getStyle = () => {
   return style
 }
 
+const ChatWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="rounded-[12px] p-2 border border-gray-500/50 bg-gray-900/50 self-end w-fit">
+      {children}
+    </div>
+  )
+}
+
 const ChatElement = (props: Chat) => {
   const { senderName, message } = props
   return (
-    <div className="rounded-[12px] p-2 border border-gray-800 self-end w-fit">
+    <ChatWrapper>
       {senderName}: {message}
-    </div>
+    </ChatWrapper>
+  )
+}
+
+const ChatSender = () => {
+  const [text, setText] = useState("")
+  const [collapsed, setCollapsed] = useStorage("collapsed", true)
+
+  const suppress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    e.stopPropagation()
+  }
+
+  const sendChat = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    message("chat/send", { text })
+    setText("")
+  }
+
+  return (
+    <ChatWrapper>
+      <div className="flex gap-4 h-8">
+        {!collapsed && (
+          <form onSubmit={sendChat} className="flex gap-4">
+            <input
+              className="h-8 ps-1 text-white bg-transparent border-none outline-none placeholder:text-white w-[15rem]"
+              placeholder="채팅 내용을 입력"
+              onKeyDown={suppress}
+              onKeyUp={suppress}
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            />
+            <button type="submit" className="cursor-pointer">
+              <LuSend size={32} />
+            </button>
+          </form>
+        )}
+        <LuMessageSquare
+          size={32}
+          onClick={() => setCollapsed(!collapsed)}
+          className="cursor-pointer"
+        />
+      </div>
+    </ChatWrapper>
   )
 }
 
@@ -36,13 +87,7 @@ const Chatting = () => {
   // todo : suppress keypress event (fullscreen, etc)
   return (
     <div className="absolute top-4 right-4 flex flex-col gap-4">
-      {/* <div className="rounded-[12px] h-10 p-1 flex gap-2 border border-gray-800 w-fit self-end">
-        <input
-          className="h-8 text-white bg-transparent border-none outline-none"
-          placeholder="채팅 내용을 입력"
-        />
-        <LuMessageSquare size={32} />
-      </div> */}
+      <ChatSender />
       {chatType !== "none" &&
         chat &&
         chat.map((v: Chat, i: number) => <ChatElement key={i} {...v} />)}
