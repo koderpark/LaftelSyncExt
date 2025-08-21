6f09c748212a589487ea94ff1e725e37b1065641
박성훈 / Koder <koder0205@gmail.com>
2025-08-21 20:51:46 +0900

    fix: log at client side

--- src/background/service/log.ts
+++ src/background/service/log.ts
@@ -1,33 +1,15 @@
 import { Storage } from "@plasmohq/storage"
-import type { Log } from "../const"
 const storage = new Storage()
 
 export const logModule = (() => {
-  const get = async (): Promise<Log[]> => {
-    return (await storage.get("log")) || []
-  }
-
-  const set = async (input: Log[]) => {
-    await storage.set("log", input)
-  }
-
-  const push = async (input: Log) => {
-    const list = await get()
-    list.push(input)
-    await set(list)
-
-    setTimeout(async () => {
-      const list = await get()
-      list.shift()
-      await set(list)
-    }, 2500)
-  }
-
   const log = async (type: "success" | "error", msg: string) => {
-    await push({
-      type,
-      message: msg,
-      time: new Date()
+    chrome.runtime.sendMessage({
+      action: "log",
+      payload: {
+        type,
+        message: msg,
+        time: new Date()
+      }
     })
   }
 
