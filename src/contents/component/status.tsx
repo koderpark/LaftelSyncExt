e9db03d3ffcdb895996a9a655a7fdb6abe8cc541
박성훈 / Koder <koder0205@gmail.com>
2025-08-30 10:47:55 +0900

    fix: update 30sec interval

--- src/contents/component/status.tsx
+++ src/contents/component/status.tsx
@@ -1,12 +1,15 @@
 import { sendToBackground } from "@plasmohq/messaging"
+import { useEffect } from "react"
 
 export const Parser = () => {
-  const vid = document.querySelector("video")
-
-  vid?.addEventListener("canplay", parseVideo)
-  vid?.addEventListener("ratechange", parseVideo)
-  vid?.addEventListener("pause", parseVideo)
-  vid?.addEventListener("play", parseVideo)
+  useEffect(() => {
+    const vid = document.querySelector("video")
+    vid?.addEventListener("canplay", parseVideo)
+    vid?.addEventListener("ratechange", parseVideo)
+    vid?.addEventListener("pause", parseVideo)
+    vid?.addEventListener("play", parseVideo)
+    setInterval(parseVideo, 30000)
+  }, [])
 
   return (
     <div className="bg-green-500 absolute top-2 left-2 size-2 rounded-full"></div>
