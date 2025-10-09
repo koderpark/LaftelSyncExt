3538e648b5ca6ee3353318dc1035ebe1773d2105
박성훈 / Koder <koder0205@gmail.com>
2025-10-10 07:19:42 +0900

    fix: scroll

--- src/sidepanel/index.tsx
+++ src/sidepanel/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { Storage } from "@plasmohq/storage"
 import { useStorage } from "@plasmohq/storage/hook"
 import type { Chat } from "~const"
@@ -33,6 +33,12 @@ function IndexSidePanel() {
       area: "local"
     })
   })
+  const chatRef = useRef<HTMLDivElement>(null)
+  useEffect(() => {
+    if (chatRef.current) {
+      chatRef.current.scrollIntoView({ behavior: "smooth" })
+    }
+  }, [chat])
 
   const sendChat = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
@@ -41,15 +47,16 @@ function IndexSidePanel() {
   }
 
   return (
-    <div className="bg-gray-700 p-4 flex flex-col gap-4 h-screen w-full text-white">
+    <div className="bg-gray-950 p-4 flex flex-col gap-4 h-screen w-full text-white">
       <h1 className="text-3xl font-bold">채팅 패널</h1>
-      <div className="flex flex-col gap-2 grow w-full bg-gray-800 rounded-[12px] p-4 overflow-y-scroll">
+      <div className="flex flex-col gap-2 grow w-full bg-gray-900 rounded-[12px] p-4 overflow-y-scroll">
         {chat &&
           chat.map((v: Chat, i: number) => <ChatElement key={i} {...v} />)}
+        <div ref={chatRef}></div>
       </div>
       <form
         onSubmit={sendChat}
-        className="flex gap-2 bg-gray-800 rounded-[12px] p-1 w-full h-16">
+        className="flex gap-2 bg-gray-900 rounded-[12px] p-1 w-full h-16">
         <StringField label="채팅 내용" value={text} setValue={setText} />
         <Btn label="전송" type="submit" padding="small" />
       </form>
